86a4d5f0355a7731ca968ce843e59c04f3ba4550
Obakeng <obeezus@gmail.com>
2019-09-16 10:12:17 +0200

    second commit

--- calculator.js
+++ calculator.js
@@ -1,22 +1,21 @@
 module.exports = class Calculator {
   constructor() {
-    this.result;
+    this.result = 0;
   }
   //returns the sum of added numbers
   add() {
-    let result = 0;
     for (let i = 0; i < arguments.length; i++) {
-      result += arguments[i];
+      this.result += arguments[i];
     }
-    return result;
+    return this.result;
   }
   //returns the sum of multiples
   multiply() {
-    let result = arguments[0];
+    this.result = arguments[0];
 
     for (let i = 0; i < arguments.length; i++) {
-      result *= arguments[i];
+      this.result *= arguments[i];
     }
-    return result;
+    return this.result;
   }
 };
